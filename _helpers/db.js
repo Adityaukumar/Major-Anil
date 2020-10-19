b15d2345a6197dbb56e2c0ab8ec9792f134b16db
master <kumaraditya9630@gmail.com>
2020-10-19 20:23:23 +0530

    updated environment

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,5 +1,5 @@
 const config = require('config.json');
-const mongoose = require('mongoose');        
+const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || config.connectionString, { useCreateIndex: true, useNewUrlParser: true })
     .then(() => {
         console.log(process.env.MONGODB_URI || config.connectionString)
@@ -12,10 +12,10 @@ mongoose.Promise = global.Promise;
 module.exports = {
     User: require('../models/user.model'),
     Hospital: require('../models/hospital.model'),
-    Request:require('../models/request.model'),
-    Orequest:require('../models/orequest.model'),
-    Contact:require('../models/contact.model'),
-    Appointment:require('../models/appointment.model')
+    Request: require('../models/request.model'),
+    Orequest: require('../models/orequest.model'),
+    Contact: require('../models/contact.model'),
+    Appointment: require('../models/appointment.model')
 };
 
 // connection_string:"mongodb://localhost/major"
